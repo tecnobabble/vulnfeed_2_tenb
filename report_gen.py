5160ab17af40f6e3775b8c2c6104cff3ca929ed9
Seth <seth.matheson@gmail.com>
2020-06-19 11:27:29 -0700

    working report generation

--- report_gen.py
+++ report_gen.py
@@ -1,62 +1,50 @@
 #!/usr/bin/env python
 
-#Import necessary functions from Jinja2 module
 from jinja2 import Environment, FileSystemLoader
-
-#Import YAML module
-#import yaml
-
 import re
-from phpserialize import * #serialize, unserialize
+from phpserialize import serialize, unserialize
 import base64
 import ast
 
-
-#Load Jinja2 template
-
-sc_template = open("templates/sc_template.xml", "rt") # open lorem.txt for reading text
-template_contents = sc_template.read()         # read the entire file into a string
-
+# Let's read the base sc template and pull out the report definition
+sc_template_file = open("templates/sc_template.xml", "r")
+template_contents = sc_template_file.read()
 template_def = re.search("<definition>(.+)</definition>", str(template_contents))
+sc_template_file.close()
+
+# Let's put the encoded report def into a format we can work with
 template_def = base64.b64decode(template_def.group(1))
 template_def = unserialize(template_def, decode_strings=True)
 
+# Replace the CVE placeholder with something we can swap out later
 template_def = str(template_def).replace("CVE-1990-0000", "{{ cve_list }}")
 
+# Write this definition template to a file
+template_def_file = open("templates/definition.txt", "w")
+template_def_file.write(template_def)
+template_def_file.close()
 
-
-
-#template_def = ast.literal_eval(template_def)
-
-#template = serialize(template_def)
-
-#print(template)
-
-#print(template_def)
-
-#myfile.close() 
-
+# Load the definition template as a jinja template
 env = Environment(loader = FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
 template_def = env.get_template('definition.txt')
 
+#cve_list = ','.join(cve_list)
 
-Entry_Title = "Top 10 Routinely Exploited Vulnerabilities"
-cve_list = ['CVE-2017-11882', 'CVE-2017-0199', 'CVE-2017-5638', 'CVE-2012-0158', 'CVE-2019-0604', 'CVE-2017-0143', 'CVE-2018-4878', 'CVE-2017-8759', 'CVE-2015-1641', 'CVE-2018-7600', 'CVE-2019-19781', 'CVE-2019-11510']
+Entry_Title = "test title"
+cve_list = "CVE-2020-0001"
 
-cve_list = ','.join(cve_list)
-
-#Render the template with data and print the output
+#Render the definition template with data and print the output
 report_raw = template_def.render(Entry_Title=Entry_Title, cve_list=cve_list)
 
+# Convert the now rendered template back into a format that tsc can understand (base64 encoded PHP serilaized string)
 report_raw = ast.literal_eval(report_raw)
-#print(report_raw)
 report_output = base64.b64encode(serialize(report_raw))
 
-#print(report_output)
-#print(report_output.decode('utf8'))
-
+# Render the full XML report template and write the output to a file that we'll then upload to tsc.
 report = env.get_template('report.txt')
 report_xml = report.render(Entry_Title=Entry_Title, report_output=report_output.decode('utf8'))
+generated_tsc_report_file = open("tsc_report.xml", "w")
+generated_tsc_report_file.write(report_xml)
+generated_tsc_report_file.close()
 
-print(report_xml)
 
